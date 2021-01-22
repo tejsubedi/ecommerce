0d29b798f1f66f13a5bcfadf516218e31a683671
tejsubedi@gmail.com <tejsubedi@gmail.com>
2021-01-21 22:03:07 -0500

    ecommerce website

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+//import routes
+const userRoutes = require('./routes/user')
+
+//app
+const app = express()
+
+
+//database
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("DB Connected")
+});
+
+app.use('/api', userRoutes);
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server is running in port ${port}`)
+});
