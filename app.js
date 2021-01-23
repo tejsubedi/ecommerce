098c3ce27a47375402c39e4d06fa8cfe978500e3
tejsubedi@gmail.com <tejsubedi@gmail.com>
2021-01-23 16:31:54 -0500

    virtual model added

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 
 //import routes
-const userRoutes = require('./routes/user')
+const userRoutes = require('./routes/user');
+
 
 //app
 const app = express()
@@ -18,6 +22,11 @@ mongoose.connect(process.env.DATABASE, {
     console.log("DB Connected")
 });
 
+
+//middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
 app.use('/api', userRoutes);
 
 const port = process.env.PORT || 8000
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto')
-const uuidv1 = require('uuid/v1');
+const uuidv1 = require('uuidv1')
+
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -38,4 +39,28 @@ const userSchema = new mongoose.Schema({
 
 
 //virtual field
-userSchema.virtual('password').set().get()
+userSchema.virtual('password')
+    .set(function (password) {
+        this._password = password
+        this.salt = uuidv1()
+        this.hashed_passwrod = this.encryptPassword(password)
+    })
+    .get(function () {
+        return this._password
+    })
+
+userSchema.methods = {
+    encryptPassword: function (password) {
+        if (!password) return '';
+        try {
+            return crypto.createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+        } catch (err) {
+            return "";
+        }
+    }
+}
+
+
+module.exports = mongoose.model("User", userSchema);
